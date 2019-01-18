bde8271e34f00b28fd777bb33a0a7b7e5b2e4f8e
DesignDocJockey <hwong18@gmail.com>
2019-01-18 00:00:43 -0500

    Added models folder

---
+++ models/quarterback.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//with no default id created
+// const quarterbackSchema = new mongoose.Schema({
+//     number: Number,
+//     firstName: String,
+//     lastName: String,
+//     date: { type: Date, default: Date.now},
+//     tags: [String]
+// }, {_id: false});
+
+const quarterbackSchema = new mongoose.Schema({
+    number: Number,
+    firstName: String,
+    lastName: String,
+    dob: { type: Date },
+    weight: Number,
+    college: String,
+    createdOn: { type: Date, default: Date.now},
+    teams: [String]
+});
+
+module.exports = quarterbackSchema;
