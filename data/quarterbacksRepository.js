48a56d5b9e1c476645a863b01236a0b7302a634a
DesignDocJockey <hwong18@gmail.com>
2019-03-31 13:49:57 -0400

    Fixed code to use async

--- data/quarterbacksRepository.js
+++ data/quarterbacksRepository.js
@@ -3,10 +3,6 @@
 const mongoose = require('mongoose');
 //const config = require('config');
 
-mongoose.connect('mongodb://localhost/players')
-    .then( () => console.log(`Connected to MongoDB Instance`) )
-    .catch( err => console.error(`Could not connect to MongoDB Instance`, err));
-
 //schema defines the structure of the MongoDB documents i.e. properties & fields
 const quarterbackSchema = new mongoose.Schema({
     number: Number,
@@ -66,18 +62,31 @@ async function updateQuarterback(id, number, first, last, tags) {
 async function removeQuarterback(id) {
     const qb = await Quarterback.findOneAndRemove(id);
 }
+async function testMeOut () {
+    try {
+        const dbConnection = await mongoose.connect('mongodb://localhost/players');
+        console.log(`Connected to MongoDB Instance`);
+        const qbs = await getAllQuarterbacks();
+        console.log(`Retrieving all QBs`);
+        console.log(JSON.stringify(qbs, undefined, 1));
 
-getQuarterbacks().then(
-    (qb) => {
-        console.log(JSON.stringify(qb, undefined, 1));
+        console.log(`Creating a QB...`);
+        const newlyCreatedQb =  await createQuarterback(8, 'Kirk', 'Cousins',  ['Quarterback', "Vikings"] );
+        console.log(`Updating a QB...`);
+        await updateQuarterback(newlyCreatedQb.id, 15, "Patrick", "Mahomes",  ["Chiefs", "Quarterback", "Kermit"]);
+
+    } catch( err) {
+        console.error(`Could not connect to MongoDB Instance`, err);
     }
-);
+}
 
-createQuarterback(8, 'Kirk', 'Cousins',  ['Quarterback', "Vikings"] )
-    .then((newlyCreatedQb) => {
-        updateQuarterback(newlyCreatedQb.id, 15, "Patrick", "Mahomes",  ["Chiefs", "Quarterback", "Kermit"])
-    });
+testMeOut();
 
+// getQuarterbacks().then(
+//     (qb) => {
+//       
+//     }
+// );
 /**
         number: 12,
         firstName: 'Tom',
